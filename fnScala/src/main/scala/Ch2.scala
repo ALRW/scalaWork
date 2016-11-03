b7c8a4766915bbb5be417b719269f982c1998ab5
ALRW <awerner1@googlemail.com>
2016-11-03 16:43:36 +0000

    [AW] added isSorted and curry

--- fnScala/src/main/scala/Ch2.scala
+++ fnScala/src/main/scala/Ch2.scala
@@ -1,6 +1,7 @@
 import scala.annotation.tailrec
 
 object Ch2 {
+
   def fibonacci(n: Int): Int = {
     @tailrec
     def increment(inc: Int, previous: Int, current: Int): Int ={
@@ -8,5 +9,13 @@ object Ch2 {
       else increment(inc + 1, current, previous + current)
     }
     increment(1,0,1)
+  }
+
+  def isSorted[A](as: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    as sameElements as.sortWith(ordered)
+  }
+
+  def curry[A,B,C](fn: (A,B) => C): A => (B => C) = {
+    (a: A) => (b: B) => fn(a,b)
   }
 }
--- fnScala/src/test/scala/Ch2Spec.scala
+++ fnScala/src/test/scala/Ch2Spec.scala
@@ -9,5 +9,26 @@ class Ch2Spec extends FunSpec with Matchers {
     }
   }
 
+  describe("#isSorted") {
+    it("should return false if an array is not sorted according to a given function") {
+      val a = Array[Int](1,3,5,4,2,6,9,2)
+      val fn = (a:Int,b:Int) => a > b
+      assert(!Ch2.isSorted(a, fn))
+    }
+    it("should return true if an array is sorted according to a given function") {
+      val a = Array[String]("a", "b", "c")
+      val fn = (a: String, b: String) => a < b
+      assert(Ch2.isSorted(a,fn))
+    }
+  }
+
+  describe("#curry") {
+    it("should take a function of two arguments and returns a partially applied funciton"){
+      val fn1 = (a: Int, b:Float) => a.toByte + b.toByte
+      val fn2 = (a: Int) => (b: Float) => a.toByte + b.toByte
+      assert(Ch2.curry(fn1)(1)(2) == fn2(1)(2))
+    }
+  }
+
 
 }
