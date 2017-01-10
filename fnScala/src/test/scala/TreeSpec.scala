3079415cbbc1dec0fa56a5f2b57c7295fbed691a
ALRW <awerner1@googlemail.com>
2017-01-10 07:27:56 +0000

    [AW] added size method to tree

--- fnScala/src/test/scala/TreeSpec.scala
+++ fnScala/src/test/scala/TreeSpec.scala
@@ -2,5 +2,13 @@ import org.scalatest.{FunSpec, Matchers}
 
 class TreeSpec extends FunSpec with Matchers {
 
+  describe("#size"){
+    it("should return the number of nodes (branches and leaves on a tree"){
+      assert(Tree.size(Branch(Branch(Leaf(1), Leaf(2)), Leaf(3))) == 5)
+    }
+    it("should work for larger Trees"){
+      assert(Tree.size(Branch(Branch(Branch(Branch(Leaf(1), Leaf(2)), Leaf(3)), Leaf(4)), Leaf(5))) == 9)
+    }
+  }
 
 }
