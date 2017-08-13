1116d37557da268f9b76f3dbcbb4c73d022f921d
ALRW <awerner1@googlemail.com>
2017-08-13 18:51:39 +0100

    [AW] implemented all remaining RNG methods

--- fnScala/src/main/scala/RNG.scala
+++ fnScala/src/main/scala/RNG.scala
@@ -2,32 +2,34 @@ trait RNG {
   def nextInt: (Int, RNG)
 }
 
+object RNG {
 
-case class SimpleRNG(seed: Long) extends RNG {
+  case class SimpleRNG(seed: Long) extends RNG {
+
+    def nextInt: (Int, RNG) = {
+      val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+      val nextRNG = SimpleRNG(newSeed)
+      val n = (newSeed >>> 16).toInt
+      (n, nextRNG)
+    }
+  }
 
   type Rand[+A] = RNG => (A, RNG)
-
-  def nextInt: (Int, RNG) = {
-    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
-    val nextRNG = SimpleRNG(newSeed)
-    val n = (newSeed >>> 16).toInt
-    (n, nextRNG)
-  }
 
   def randomPair(rng: RNG): ((Int, Int), RNG) = {
     val (i1, rn) = rng.nextInt
     val (i2, nrn) = rn.nextInt
-    ((i1,i2), nrn)
+    ((i1, i2), nrn)
   }
 
   def nonNegativeInt(rng: RNG): (Int, RNG) = {
-    val (i, nrs) = nextInt
-    (if(i < 0) -(i + 1) else i, nrs)
+    val (i, nrs) = rng.nextInt
+    (if (i < 0) -(i + 1) else i, nrs)
   }
 
   def double(rng: RNG): (Double, RNG) = {
     val (i, nrs) = nonNegativeInt(rng)
-    (i / (Int.MaxValue + 1), nrs)
+    (i / (Int.MaxValue.toDouble + 1), nrs)
   }
 
   def intDouble(rng: RNG): ((Int, Double), RNG) = {
@@ -38,7 +40,7 @@ case class SimpleRNG(seed: Long) extends RNG {
 
   def doubleInt(rng: RNG): ((Double, Int), RNG) = {
     val ((x, y), nrng) = intDouble(rng)
-    ((y,x), nrng)
+    ((y, x), nrng)
   }
 
   def double3(rng: RNG): ((Double, Double, Double), RNG) = {
@@ -48,12 +50,62 @@ case class SimpleRNG(seed: Long) extends RNG {
     ((x, y, z), fnrng)
   }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) ={
-    if(count == 0) (List(), rng)
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    if (count == 0) (List(), rng)
     else {
       val (x, nrng) = rng.nextInt
       val (xs, nnrng) = ints(count - 1)(nrng)
       (Cons(x, xs), nnrng)
-      }
+    }
   }
+
+  def int: Rand[Int] = _.nextInt
+
+  def unit[A](a: A): Rand[A] = rng => (a, rng)
+
+  def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    rng => {
+      val (a, rng2) = s(rng)
+      (f(a), rng2)
+    }
+
+  def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
+
+  def mapDouble: Rand[Double] = map(nonNegativeInt)(i => i / (Int.MaxValue.toDouble + 1))
+
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    rng => {
+      val (a, r) = ra(rng)
+      val (b, rr) = rb(r)
+      (f(a, b), rr)
+    }
+
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] =
+    map2(ra, rb)((_, _))
+
+  val randIntDouble: Rand[(Int, Double)] = both(int, double)
+
+  val randDoubleInt: Rand[(Double, Int)] = both(double, int)
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] =
+    List.foldright(fs, unit(List[A]()))((a, b) => map2(a, b)((c, d) => Cons(c, d)))
+
+  def nonNegativeLessThan(n: Int) : Rand[Int] =
+    flatMap(nonNegativeInt) { i =>
+      val mod = i % n
+      if(i + (n -1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+    }
+
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] =
+    rng => {
+      val (a, r1) = f(rng)
+      g(a)(r1)
+    }
+
+  def flatReMap[A, B](s: Rand[A])(f: A => B): Rand[B] =
+    flatMap(s)(r => unit(f(a)))
+
+  def flatReMap2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] =
+    flatMap(ra)(a => map(rb)(b => f(a,b)))
 }
+
--- fnScala/src/test/scala/RNGSpec.scala
+++ fnScala/src/test/scala/RNGSpec.scala
@@ -1,12 +1,12 @@
+import RNG.SimpleRNG
 import org.scalatest.{FunSpec, Matchers}
 
 class RNGSpec extends FunSpec with Matchers {
 
-  val rng = SimpleRNG(1)
+
   describe("#nonNegativeInt") {
     it("should generate a positive random number") {
-
-      val randomInt = rng.nonNegativeInt(rng)._1
+      val randomInt = RNG.nonNegativeInt(SimpleRNG(1))._1
       randomInt should be < Int.MaxValue
       randomInt should be > 0
     }
@@ -14,23 +14,21 @@ class RNGSpec extends FunSpec with Matchers {
 
   describe("#double") {
     it("returns a random double between 0 and 1 exlclusive") {
-
-      val randomDouble = rng.double(rng)._1
-      randomDouble should be < 1.0
-      randomDouble should be >= 0.0
+      val (a, _) = RNG.double(SimpleRNG(3))
+      a shouldBe 5.374876782298088E-4
     }
   }
 
   describe("#intDouble") {
     it("provides an Int Double pair") {
-      val randomid = rng.intDouble(rng)._1
-      randomid should matchPattern { case Tuple2(_: Int, _: Double) => }
+      val (a, _) = RNG.intDouble(SimpleRNG(3))
+      a should matchPattern { case Tuple2(_: Int, _: Double) => }
     }
   }
 
   describe("#doubleInt") {
     it("provides a Double Int pair") {
-      val (x, y) = rng.doubleInt(rng)._1
+      val (x, y) = RNG.doubleInt(SimpleRNG(2))._1
       x should matchPattern { case _: Double => }
       y should matchPattern { case _: Int => }
     }
@@ -38,16 +36,32 @@ class RNGSpec extends FunSpec with Matchers {
 
   describe("#double3") {
     it("provides a tuple3 of Doubles") {
-      val rd3 = rng.double3(rng)._1
-      rd3 should matchPattern { case Tuple3(_: Double, _: Double, _: Double) => }
+      val (a, _) = RNG.double3(SimpleRNG(2))
+      a should matchPattern { case Tuple3(_: Double, _: Double, _: Double) => }
     }
   }
 
   describe("#ints") {
     it("generate a list of random integers") {
-      val (l, nrng) = rng.ints(2)(rng)
+      val (l, _) = RNG.ints(2)(SimpleRNG(5))
       List.length(l) shouldBe 2
     }
   }
+
+  describe("#mapDouble") {
+    it("returns a random double between 0 and 1 exclusive using map for its implementation") {
+      val (a, _) = RNG.mapDouble(SimpleRNG(42))
+      a shouldBe 0.007524831686168909
+    }
+  }
+
+  describe("#map2") {
+    it("takes two actions and apply's a function to combine their results and returns a new action from the combination") {
+      val (a, _) = RNG.map2(RNG.int, RNG.mapDouble)((_,_))(SimpleRNG(22))
+      a shouldBe (8464475,0.16455321665853262)
+    }
+  }
+
+
 
 }
