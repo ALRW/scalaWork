d07a040a10eecf8b9a8d41451f1d56a08f26a4ba
ALRW <awerner1@googlemail.com>
2017-09-25 10:12:08 +0100

    [AW] added initial concurrecy project

---
+++ concurrency/build.sbt
@@ -0,0 +1,6 @@
+name := "concurrency"
+
+version := "0.1"
+
+scalaVersion := "2.12.3"
+        
---
+++ concurrency/src/main/scala/Par.scala
@@ -0,0 +1,6 @@
+abstract class Par[A] {
+
+  def unit[A](a: => A): Par[A]
+
+  def get[A](a: Par[A]): A
+}
