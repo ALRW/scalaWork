0df4dd657a732d1b2e5a084e249d06aefafe3d94
ALRW <awerner1@googlemail.com>
2017-09-25 10:48:26 +0100

    [AW] added initial implementation

--- concurrency/src/main/scala/Par.scala
+++ concurrency/src/main/scala/Par.scala
@@ -1,17 +1,36 @@
-import java.util.concurrent.{ExecutorService, Future}
+import java.util.concurrent.{Callable, ExecutorService, Future, TimeUnit}
 
 object Par {
 
   type Par[A] = ExecutorService => Future[A]
 
-  def unit[A](a: A): Par[A]
+  def unit[A](a: A): Par[A] = (es: ExecutorService) => UnitFuture(a)
 
   def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
 
-  def run[A](a: Par[A]): A
+  def run[A](s: ExecutorService)(a: Par[A]): Future[A] = a(s)
 
-  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C]
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C] = (es: ExecutorService) => {
+    val af = a(es)
+    val bf = b(es)
+    UnitFuture(f(af.get, bf.get))
+  }
 
-  def fork[A](a: => Par[A]): Par[A]
+  def fork[A](a: => Par[A]): Par[A] = (es: ExecutorService) => {
+    es.submit(new Callable[A] {
+      def call: A = a(es).get
+    })
+  }
+
+  private case class UnitFuture[A](get: A) extends Future[A] {
+    def isDone = true
+
+    def get(timeout: Long, units: TimeUnit): A = get
+
+    def isCancelled = false
+
+    def cancel(evenIfRunning: Boolean): Boolean = false
+  }
+
 }
 
