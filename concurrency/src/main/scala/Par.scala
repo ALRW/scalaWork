967caf4996def576ef2ea4da9240ab4b71ba7c34
ALRW <awerner1@googlemail.com>
2017-09-25 10:37:51 +0100

    [AW] added abstract representation of API

--- concurrency/src/main/scala/Par.scala
+++ concurrency/src/main/scala/Par.scala
@@ -1,6 +1,17 @@
-abstract class Par[A] {
+import java.util.concurrent.{ExecutorService, Future}
 
-  def unit[A](a: => A): Par[A]
+object Par {
 
-  def get[A](a: Par[A]): A
+  type Par[A] = ExecutorService => Future[A]
+
+  def unit[A](a: A): Par[A]
+
+  def lazyUnit[A](a: => A): Par[A] = fork(unit(a))
+
+  def run[A](a: Par[A]): A
+
+  def map2[A, B, C](a: Par[A], b: Par[B])(f: (A, B) => C): Par[C]
+
+  def fork[A](a: => Par[A]): Par[A]
 }
+
