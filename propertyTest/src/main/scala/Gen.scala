f343fb274ed031ea87fb43d1f68ad5b0ac07448a
ALRW <awerner1@googlemail.com>
2017-09-27 08:42:42 +0100

    [AW] added RNG to propTest library

--- propertyTest/src/main/scala/Gen.scala
+++ propertyTest/src/main/scala/Gen.scala
@@ -1,3 +1,5 @@
+import Prop.{FailedCase, SuccessCount}
+
 trait Gen[A] {
 
   def listOf[A](a: Gen[A]): Gen[List[A]]
@@ -8,9 +10,12 @@ trait Gen[A] {
 
 }
 
+object Prop {
+  type SuccessCount = Int
+  type FailedCase = String
+}
 trait Prop {
 
-  def check: Boolean
+  def check: Either[(FailedCase, SuccessCount), SuccessCount]
 
-  def &&(p: Prop): Boolean = this.check && p.check
 }
